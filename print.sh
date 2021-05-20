8ecfa9b7649c4f164b0005637e18cb59a69eea10
srikanthbejjipuram <sreebjprm@gmail.com>
2021-05-20 17:52:33 +0530

    updated file2

--- print.sh
+++ print.sh
@@ -17,6 +17,11 @@ echo -e "Hai/nBye"
 echo -e "\e[31mWelcome to training"
 
 
+echo -e "\e[32mWelcome to training"
+
 echo -e "\e[33mWelcome to training"
 
 echo -e "\e[34mWelcome to training"
+
+echo -e "\e[35mWelcome to training"
+
