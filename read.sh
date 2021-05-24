f2fb89e2af86556df83f7d77bc8f298ecf04b537
srikanthbejjipuram <sreebjprm@gmail.com>
2021-05-24 20:00:45 +0530

    updated

--- read.sh
+++ read.sh
@@ -4,5 +4,9 @@ read -p 'enter your name:' name
 
 read -p 'enter your age:' age
 
-echo "your name = $name, your age = $age"
+read -p 'enter your hometown name:' hometown
+
+read -p 'enter your mother name:' mother
+
+echo "your name = $name, your age = $age, enter your hometown = $hometown, mother name = $mother"
 
